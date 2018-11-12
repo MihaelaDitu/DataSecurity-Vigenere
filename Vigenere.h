66e8ecd9b60e49795e88bbda5bc315310ccaa116
MihaelaDitu <37001863+MihaelaDitu@users.noreply.github.com>
2018-11-12 22:54:35 +0200

    Add files via upload

---
+++ Vigenere.h
@@ -0,0 +1,7 @@
+#ifndef VIGENERE_H_INCLUDED
+#define VIGENERE_H_INCLUDED
+
+void encrypt(char cipher[], char keyword[]);
+void decrypt(char cipher[], char keyword[]);
+
+#endif // VIGENERE_H_INCLUDED
